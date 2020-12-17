6d663cfe188fd0977b9e8465ef81cecaaf2bb5df
sergeymild <sergeymild@yandex.ru>
2020-12-17 16:27:39 +0200

    initial

---
+++ knex.js
@@ -0,0 +1,11 @@
+// Knex.js
+// --------------
+//     (c) 2013-present Tim Griesser
+//     Knex may be freely distributed under the MIT license.
+//     For details and documentation:
+//     http://knexjs.org
+
+import Knex from './lib/knex';
+import NativeClient from './lib/dialects/sqlite3/sqlite-native'
+export { NativeClient }
+export default Knex
